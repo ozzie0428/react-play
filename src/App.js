6a4775ef5066e6149f172d7406f3733fa57ccf4d
DenimCity <altidorj1@gmail.com>
2019-12-31 14:49:18 -0500

    setting up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 import Welcome1 from './components/Landing/Welcome';
 import Toodles, { SayHelloBye, GoodBye } from './components/Landing/Goodbye';
@@ -15,22 +15,35 @@ class Welcome extends Component {
 		firstName: 'Jean',
 		lastName: 'Altidor',
 		addresses: [ { streetName: '701 Tuxwroth' }, { streetName: '545 eagle Drive' } ],
-		newName: ''
+		newName: '',
+		jobName: ''
+	};
+
+	test(params) {
+		// console.log("hi", params ? params : "Ozzie");
+		console.log('hi', 'Ozzie');
+	}
+
+	onSubmit = (event) => {
+		event.preventDefault();
+		window.alert(JSON.stringify(this.state, null, 3));
+	};
+
+	onChange = (event) => {
+		const inputfield = [ event.target.name ];
+		const inputValue = event.target.value;
+		this.setState({ [inputfield]: inputValue });
 	};
 
 	render() {
-		function test(params) {
-			// console.log("hi", params ? params : "Ozzie");
-			console.log('hi', 'Ozzie');
-		}
-
-		function onSubmit() {}
+		const { firstName, lastName, newName, jobName } = this.state;
 		return (
 			<div>
-				<button onClick={test}>Add</button>
-				<h1>{this.state.firstName}</h1>
-				<h1>{this.state.lastName}</h1>
-				<h1>{this.state.newName ? this.state.newName : 'No Name Entered Yet'}</h1>
+				<button onClick={this.test}>Add</button>
+				<h1>{firstName}</h1>
+				<h1>{lastName}</h1>
+				<h1>{newName ? newName : 'No Name Entered Yet'}</h1>
+				<h1>{jobName ? jobName : 'No jobName Entered Yet'}</h1>
 				{/* {this.state.addresses.map((k, index) => {
 					return <h2 key={`${index}-${k.streetName}`}>{k.streetName}</h2>;
 				})} */}
@@ -39,22 +52,25 @@ class Welcome extends Component {
 						Name:
 						<input
 							type="text"
-							name="name"
+							name="newName"
+							title="newName"
 							value={this.state.newName}
 							placeholder="Enter Your Name"
-							onChange={(event) => {
-								// console.log(event.target.value);
-								this.setState({ newName: event.target.value });
-							}}
+							onChange={this.onChange}
 						/>
 					</label>
-					<button
-						type="submit"
-						onClick={(event) => {
-							event.preventDefault();
-							window.alert(JSON.stringify(this.state, null, 3));
-						}}
-					>
+					<label>
+						Job:
+						<input
+							type="text"
+							name="jobName"
+							title="jobName"
+							value={this.state.jobName}
+							placeholder="Enter Your Job title"
+							onChange={this.onChange}
+						/>
+					</label>
+					<button type="submit" onClick={(event) => this.onSubmit(event)}>
 						Submit
 					</button>
 				</form>
@@ -66,14 +82,42 @@ class Welcome extends Component {
 }
 
 function App() {
+	const [ state, setState ] = useState({
+		latestRocket: ''
+	});
+	const helloButton = () => {
+		console.log('hi ozzie');
+	};
+
+	const spaceXApi = () => {
+		fetch('https://api.spacexdata.com/v3/launches/latest')
+			.then((response) => response.json())
+			.then((data) => setState({ latestRocket: data }));
+	};
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<Welcome />
-				{/* <Welcome1 />
-				<GoodBye />
-				<SayHelloBye />
+				{/* <Welcome /> */}
+				<Welcome1
+					company={'Best Buy'}
+					isAgirl={true}
+					cartype={'Toyota'}
+					helloButton={helloButton}
+					spaceX={spaceXApi}
+				/>
+				<GoodBye helloButton={helloButton} />
+
+				{/* <pre>{JSON.stringify(state)}</pre> */}
+				{state.latestRocket ? (
+					<div>
+						<h1>{state.latestRocket.flight_number}</h1>
+						<h1>{state.latestRocket.mission_name}</h1>
+						<h1>{state.latestRocket.rocket.rocket_name}</h1>
+					</div>
+				) : null}
+
+				{/* <SayHelloBye />
 				<Toodles /> */}
 				{/* <p>
 					Edit <code>src/App.js</code> and save Ozzie.
--- src/components/Landing/Welcome.js
+++ src/components/Landing/Welcome.js
@@ -1,9 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-class Welcome1 extends Component {
-  render() {
-    return <div>its your girl Daph</div>;
-  }
-}
+// class Welcome1 extends Component {
+// 	render() {
+// 		console.log(this.props);
+// 		return (
+// 			<div>
+// 				<h4>its your girl Daph</h4>
+// 				<h4>Works at: {this.props.company}</h4>
+// 				<h4>Drives a: {this.props.cartype} </h4>
+// 			</div>
+// 		);
+// 	}
+// }
+const Welcome1 = (props) => {
+	// render() {
+	console.log(props);
+	const { company, cartype } = props;
+	return (
+		<div>
+			<h4>its your girl Daph</h4>
+			<h4>Works at: {company}</h4>
+			<h4>Drives a: {cartype} </h4>
+			<button onClick={props.helloButton}>Greet ME</button>
+			<button onClick={props.spaceX}>Call SpaceX Api</button>
+		</div>
+	);
+	// }
+};
 
 export default Welcome1;
